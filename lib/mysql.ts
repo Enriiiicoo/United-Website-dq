47f8d4f843f3084251b946a60b3aefb67dabfe21
Enriiiicoo <anasaboudrar24@gmail.com>
2025-07-04 01:49:39 +0100

    Create mysql.ts

---
+++ lib/mysql.ts
@@ -0,0 +1,9 @@
+// lib/mysql.ts
+import mysql from 'mysql2/promise';
+
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
